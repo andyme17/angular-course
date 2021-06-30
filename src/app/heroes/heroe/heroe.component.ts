f866c8f6eaec0bf33046d9a1120b431c38ec3678
andyme17 <ame171337@gmail.com>
2021-06-30 12:06:22 -0500

    feat: add HeroeComponent and show a hero

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: 'heroe.component.html'
+})
+
+export class HeroeComponent{
+  nombre: string ='Ironman';
+  edad: number = 45;
+
+  //al crear un getter se tomará como una propiedad, no como un metodo
+  get nombreCapitalizado():string{
+    return this.nombre.toUpperCase();
+  }
+
+  obtenerNombre():string{
+    return `${ this.nombre} - ${ this.edad }`;//template string
+  }
+}
