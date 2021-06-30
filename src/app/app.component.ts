54bbfbe05da33677a33710d1c95c436b26af02a4
andy_me <andrea.morales@umvel.com>
2021-06-30 08:44:45 -0500

    feat: create a basic counter in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//nombre del componente
   templateUrl: './app.component.html',
+
+  //también existe template que coloca contenido dentro de este mismo archivo
+/*template: `<h1>Esta es una prueba</h1>
+  <p>Siguiendo un curso de angular</p>`, */
+
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  title :string = 'Contador App';
+  numero: number = 10;
+  base  : number = 5;
+
+  acumular(valor:number){
+    //this hace referencia a la propiedad número de la clase
+    this.numero += valor;
+    console.log(this.numero, valor);
+  }
 }
