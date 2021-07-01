99afdf836e26328ddf11d1ec3a3d141be7c54ef5
andyme17 <ame171337@gmail.com>
2021-07-01 11:52:29 -0500

    feat: create a template for MainPageComponent and its functionality using two way data binding

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder: 14000,
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  agregar() {
+    console.log(this.nuevo);
   }
-
 }
