41b3432204791612d8c7e9cae259a8b70e18d724
andyme17 <ame171337@gmail.com>
2021-07-01 23:44:05 -0500

    feat: list characters dynamically in MainPageComponent

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -11,12 +11,33 @@ interface Personaje {
   styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
+
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ]
+
   nuevo: Personaje = {
-    nombre: 'Trucks',
-    poder: 14000,
+    nombre: '',
+    poder: 0,
   };
 
-  agregar() {
-    console.log(this.nuevo);
+  agregar(){
+    if(this.nuevo.nombre.trim().length === 0){
+      return;
+    }
+
+    this.personajes.push(this.nuevo);
+
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    };
   }
 }
